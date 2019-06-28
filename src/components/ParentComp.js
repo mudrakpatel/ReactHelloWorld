42326e306681b21580ca53bca8453a03a1a1deff
mudrakpatel <mudrakpatel96@gmail.com>
2019-06-28 16:19:30 -0400

    Pure Components

---
+++ src/components/ParentComp.js
@@ -0,0 +1,35 @@
+import React, { PureComponent, Component } from 'react';
+import PureComp from './PureComponent';
+import RegularComp from './RegularComponent';
+
+class ParentComp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+       name: "John"
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+        this.setState({
+          name: "John"
+        });
+      }, 2000);
+  }
+
+  render() {
+    console.log("ParentComp render");
+    return (
+      <div>
+        Parent Component
+        <RegularComp name={this.state.name}/>
+        <PureComp name={this.state.name}/>
+      </div>
+    );
+  }
+
+}
+
+export default ParentComp;
