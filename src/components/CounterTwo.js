f937ff36da2f7e7adc8df7251459528c4e5f2dde
mudrakpatel <mudrakpatel96@gmail.com>
2019-07-26 21:50:19 -0400

    Render props - Part 2

---
+++ src/components/CounterTwo.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class CounterTwo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState(previousState => {
+      return {
+        count: previousState.count + 1
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.render(
+          this.state.count, this.incrementCount
+        )}
+      </div>
+    );
+  }
+
+}
+
+export default CounterTwo;
